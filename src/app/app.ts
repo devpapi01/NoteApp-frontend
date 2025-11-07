31957308766e927a5f0de86f8e187477897232f5
cybercoder0101 <ktanjiro537@gmail.com>
2025-11-07 23:51:12 +0100

    mise en place front-end

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,7 +4,7 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule],
+  imports: [ CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
